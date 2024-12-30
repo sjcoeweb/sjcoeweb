cedb8297c022462cfc89b109a84ebc432213d986
San Joaquin County Office of Education <enterpriseservices@sjcoe.net>
2024-12-30 13:49:55 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// create header; jquery required
+$( "body").prepend("<header class='header'></header>");
+$( ".header" ).load( "https://www.sjcoe.net/header.htm" );
+// create footer
+$( "body").append("<footer class='footer'></footer>");
+$( ".footer" ).load( "https://www.sjcoe.net/footer.htm" );
