fcfdabe47cf0743bf2591b998353c5bb12e36908
San Joaquin County Office of Education <enterpriseservices@sjcoe.net>
2025-01-02 09:37:37 -0800

    20250102 DK

--- index.js
+++
@@ -1,6 +0,0 @@
-// create header; jquery required
-$( "body").prepend("<header class='header'></header>");
-$( ".header" ).load( "https://www.sjcoe.net/header.htm" );
-// create footer
-$( "body").append("<footer class='footer'></footer>");
-$( ".footer" ).load( "https://www.sjcoe.net/footer.htm" );
